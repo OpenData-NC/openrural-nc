d28cc31b108410b967715822d3355f40bae4c9d5
Colin Copeland <copelco@caktusgroup.com>
2012-01-11 08:28:04 -0500

    add ability to specify field and group to filter on

--- openrural/management/commands/dbf.py
+++ openrural/management/commands/dbf.py
@@ -1,4 +1,5 @@
 import pprint
+from optparse import make_option
 from collections import defaultdict
 
 from django.core.management.base import BaseCommand
@@ -7,18 +8,34 @@ from ebdata.parsing import dbf
 
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option("-g", "--group", action="store", type="string",
+                    dest="group"),
+        make_option("-f", "--field", action="store", type="string",
+                    dest="field", default='TLID'),
+    )
     help = 'Inspect .dbf files'
 
     def handle(self, *args, **options):
         dbf_file = args[0]
-        needle = int(args[1])
-        db = self.load_db(dbf_file)
+        needle = args[1]
+        try:
+            needle = int(needle)
+        except:
+            pass
+        db = self.load_db(dbf_file, options)
         if needle in db:
             pprint.pprint(db[needle])
 
-    def load_db(self, dbf_file, rel_key='TLID'):
-        db = defaultdict(list)
+    def load_db(self, dbf_file, options):
+        if options['group']:
+            db = defaultdict(dict)
+        else:
+            db = defaultdict(list)
         with open(dbf_file, 'rb') as f:
             for row in dbf.dict_reader(f, strip_values=True):
-                db[row[rel_key]].append(row)
+                if options['group']:
+                    db[row[options['field']]][row[options['group']]] = row
+                else:
+                    db[row[options['field']]].append(row)
         return db
